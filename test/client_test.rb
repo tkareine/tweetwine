eb11ed9a7fb8901345d8e1c85aec00faf95fc921
Tuomas Kareinen <tkareine@gmail.com>
2009-04-22 19:36:26 +0300

    Fixed whitespace for tests.

--- test/client_test.rb
+++ test/client_test.rb
@@ -22,7 +22,6 @@ class ClientTest < Test::Unit::TestCase
           :text => "nuttin"
         }
       ]
-
       RestClient.expects(:get) \
                 .with("https://foo:bar@twitter.com/statuses/friends_timeline.json?count=20") \
                 .returns(statuses.to_json)
@@ -36,9 +35,8 @@ class ClientTest < Test::Unit::TestCase
           :created_at => Time.at(1),
           :user => { :username => "zanzibar" },
           :text => "wassup?"
-        },
+        }
       ]
-
       RestClient.expects(:get) \
                 .with("https://foo:bar@twitter.com/statuses/user_timeline/zanzibar.json?count=20") \
                 .returns(statuses.to_json)
@@ -52,9 +50,8 @@ class ClientTest < Test::Unit::TestCase
           :created_at => Time.at(1),
           :user => { :username => "zanzibar" },
           :text => "wassup?"
-        },
+        }
       ]
-
       RestClient.expects(:post) \
                 .with("https://foo:bar@twitter.com/statuses/update.json", {:status => "wondering about"}) \
                 .returns(statuses.to_json)
@@ -64,4 +61,3 @@ class ClientTest < Test::Unit::TestCase
     end
   end
 end
-
