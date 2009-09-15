5ac1a0e9d1eef83b6b9270e2b4da36714aa8b30a
Tuomas Kareinen <tkareine@gmail.com>
2009-09-16 01:30:34 +0300

    Retry connection on upon connection reset.

--- lib/tweetwine/rest_client_wrapper.rb
+++ lib/tweetwine/rest_client_wrapper.rb
@@ -6,6 +6,9 @@ module Tweetwine
   class RestClientWrapper
     instance_methods.each { |m| undef_method m unless m =~ /(^__|^send$|^object_id$)/ }
 
+    MAX_RETRIES = 3
+    RETRY_BASE_WAIT_TIMEOUT = 4
+
     def initialize(io)
       @io = io
     end
@@ -13,9 +16,22 @@ module Tweetwine
     protected
 
     def method_missing(name, *args, &block)
-      RestClient.send(name, *args, &block)
-    rescue RestClient::Exception, SocketError, SystemCallError => e
-      raise ClientError, e
+      tries = 0
+      begin
+        tries += 1
+        RestClient.send(name, *args, &block)
+      rescue Errno::ECONNRESET => e
+        if tries < MAX_RETRIES
+          timeout = RETRY_BASE_WAIT_TIMEOUT**tries
+          @io.warn("Could not connect -- retrying in #{timeout} seconds")
+          sleep timeout
+          retry
+        else
+          raise ClientError, e
+        end
+      rescue RestClient::Exception, SocketError, SystemCallError => e
+        raise ClientError, e
+      end
     end
   end
 end
