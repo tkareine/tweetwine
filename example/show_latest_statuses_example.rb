4b89466b4f12bce4c0bdee45d053051bf3f8cb3b
Tuomas Kareinen <tkareine@gmail.com>
2009-11-21 14:52:40 +0200

    Specify that 'home' is the default cmd in the example

--- example/show_latest_statuses_example.rb
+++ example/show_latest_statuses_example.rb
@@ -8,7 +8,7 @@ Feature "show the latest statuses" do
   INJECTION = 'FakeWeb.register_uri(:get, "https://foouser:barpwd@twitter.com/statuses/friends_timeline.json?count=20&page=1", :body => fixture("statuses.json"))'
 
   Scenario "see the latest statuses with colorization disabled" do
-    When "application is launched with command 'home'" do
+    When "application is launched with no command" do
       @status = launch_app("-a foouser:barpwd --no-colors", INJECTION) do |pid, stdin, stdout|
         @output = stdout.readlines
       end
@@ -26,7 +26,7 @@ Feature "show the latest statuses" do
   end
 
   Scenario "see the latest statuses with colorization enabled" do
-    When "application is launched with command 'home'" do
+    When "application is launched with no command" do
       @status = launch_app("-a foouser:barpwd --colors", INJECTION) do |pid, stdin, stdout|
         @output = stdout.readlines
       end
@@ -38,6 +38,24 @@ Feature "show the latest statuses" do
       @output[2].should == "\n"
       @output[58].should == "\e[32mradar\e[0m, 15 days ago:\n"
       @output[59].should == "Four short links: 29 September 2009 \e[36mhttp://bit.ly/dYxay\e[0m\n"
+      @output[60].should == "\n"
+      @status.exitstatus.should == 0
+    end
+  end
+
+  Scenario "the command for showing the latest statuses is the default command" do
+    When "application is launched with command 'home'" do
+      @status = launch_app("-a foouser:barpwd --no-colors home", INJECTION) do |pid, stdin, stdout|
+        @output = stdout.readlines
+      end
+    end
+
+    Then "the latest statuses are shown" do
+      @output[0].should == "pelit, 11 days ago:\n"
+      @output[1].should == "F1-kausi alkaa marraskuussa http://bit.ly/1qQwjQ\n"
+      @output[2].should == "\n"
+      @output[58].should == "radar, 15 days ago:\n"
+      @output[59].should == "Four short links: 29 September 2009 http://bit.ly/dYxay\n"
       @output[60].should == "\n"
       @status.exitstatus.should == 0
     end
