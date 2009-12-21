4b4f58a779eed2151216ceb8b854c9fd2fa7bae3
Tuomas Kareinen <tkareine@gmail.com>
2009-12-21 23:26:21 +0200

    Added friends and followers examples
    Signed-off-by: Tuomas Kareinen <tkareine@gmail.com>

---
+++ example/show_followers_example.rb
@@ -0,0 +1,23 @@
+require "example_helper"
+
+FakeWeb.register_uri(:get, "https://#{TEST_AUTH}@twitter.com/statuses/followers/#{TEST_USER}.json?page=1", :body => fixture("users.json"))
+
+Feature "show followers and their latest statuses" do
+  in_order_to "to see who follows me"
+  as_a "authenticated user"
+  i_want_to "see my followers and their latest statuses, if any"
+
+  Scenario "see followers and their latest statuses" do
+    When "application is launched 'followers' command" do
+      @output = launch_cli(%W{-a #{TEST_AUTH} --no-colors followers})
+    end
+
+    Then "my followers and their latest statuses are shown" do
+      @output[0].should == "jillv, 12 hours ago:"
+      @output[1].should == "choosing next target"
+      @output[2].should == ""
+      @output[3].should == "ham"
+      @output[4].should == nil
+    end
+  end
+end
---
+++ example/show_friends_example.rb
@@ -0,0 +1,23 @@
+require "example_helper"
+
+FakeWeb.register_uri(:get, "https://#{TEST_AUTH}@twitter.com/statuses/friends/#{TEST_USER}.json?page=1", :body => fixture("users.json"))
+
+Feature "show friends and their latest statuses" do
+  in_order_to "to see who I follow"
+  as_a "authenticated user"
+  i_want_to "see my friends and their latest statuses, if any"
+
+  Scenario "see friends and their latest statuses" do
+    When "application is launched 'friends' command" do
+      @output = launch_cli(%W{-a #{TEST_AUTH} --no-colors friends})
+    end
+
+    Then "my friends and their latest statuses are shown" do
+      @output[0].should == "jillv, 12 hours ago:"
+      @output[1].should == "choosing next target"
+      @output[2].should == ""
+      @output[3].should == "ham"
+      @output[4].should == nil
+    end
+  end
+end
