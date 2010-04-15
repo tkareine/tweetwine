ee8c3f990f7dcc57248cd8c9e51b73b6079d07f2
Tuomas Kareinen <tkareine@gmail.com>
2010-04-15 16:50:28 +0300

    Better organization of unit test helpers

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,67 +5,72 @@ require "test/unit"
 require "contest"
 require "mocha"
 
-module Tweetwine
-  module TestHelpers
-    def create_test_twitter_status_records_from_rest_api(*internal_records)
-      twitter_records = internal_records.map do |internal_record|
-        {
-          "user"                    => { "screen_name" => internal_record[:from_user] },
-          "created_at"              => internal_record[:created_at],
-          "text"                    => internal_record[:status],
-          "in_reply_to_screen_name" => internal_record[:to_user]
-        }
-      end
-      [twitter_records, internal_records]
-    end
-
-    def create_test_twitter_user_records_from_rest_api(*internal_records)
-      twitter_records = internal_records.map do |internal_record|
-        twitter_record = { "screen_name" => internal_record[:from_user] }
-        if internal_record[:status]
-          twitter_record.merge!({
-            "status" => {
-              "created_at"              => internal_record[:created_at],
-              "text"                    => internal_record[:status],
-              "in_reply_to_screen_name" => internal_record[:to_user],
-            }
-          })
-        end
-        twitter_record
-      end
-      [twitter_records, internal_records]
-    end
-
-    def create_test_twitter_records_from_search_api(*internal_records)
-      twitter_search_records = internal_records.map do |internal_record|
-        {
-          "from_user"   => internal_record[:from_user],
-          "created_at"  => internal_record[:created_at],
-          "text"        => internal_record[:status],
-          "to_user"     => internal_record[:to_user]
-        }
-      end
-      twitter_records = {
-        "results" => twitter_search_records
-      }
-      [twitter_records, internal_records]
-    end
-  end
-end
-
 Mocha::Configuration.prevent(:stubbing_non_existent_method)
 
-class TweetwineTestCase < Test::Unit::TestCase
-  include Tweetwine
-  include Tweetwine::TestHelpers
+module Tweetwine
+  module Test
+    module Helper
+      def create_test_twitter_status_records_from_rest_api(*internal_records)
+        twitter_records = internal_records.map do |internal_record|
+          {
+            "user"                    => { "screen_name" => internal_record[:from_user] },
+            "created_at"              => internal_record[:created_at],
+            "text"                    => internal_record[:status],
+            "in_reply_to_screen_name" => internal_record[:to_user]
+          }
+        end
+        [twitter_records, internal_records]
+      end
 
-  def assert_full_match(regex, str, msg = "")
-    match_data = regex.match(str)
-    assert(str == match_data.to_s, msg)
+      def create_test_twitter_user_records_from_rest_api(*internal_records)
+        twitter_records = internal_records.map do |internal_record|
+          twitter_record = { "screen_name" => internal_record[:from_user] }
+          if internal_record[:status]
+            twitter_record.merge!({
+              "status" => {
+                "created_at"              => internal_record[:created_at],
+                "text"                    => internal_record[:status],
+                "in_reply_to_screen_name" => internal_record[:to_user],
+              }
+            })
+          end
+          twitter_record
+        end
+        [twitter_records, internal_records]
+      end
+
+      def create_test_twitter_records_from_search_api(*internal_records)
+        twitter_search_records = internal_records.map do |internal_record|
+          {
+            "from_user"   => internal_record[:from_user],
+            "created_at"  => internal_record[:created_at],
+            "text"        => internal_record[:status],
+            "to_user"     => internal_record[:to_user]
+          }
+        end
+        twitter_records = {
+          "results" => twitter_search_records
+        }
+        [twitter_records, internal_records]
+      end
+    end
+
+    module Assertion
+      def assert_full_match(regex, str, msg = "")
+        match_data = regex.match(str)
+        assert(str == match_data.to_s, msg)
+      end
+
+      def assert_no_full_match(regex, str, msg = "")
+        match_data = regex.match(str)
+        assert(str != match_data.to_s, msg)
+      end
+    end
   end
 
-  def assert_no_full_match(regex, str, msg = "")
-    match_data = regex.match(str)
-    assert(str != match_data.to_s, msg)
+  class TweetwineTestCase < ::Test::Unit::TestCase
+    include Tweetwine
+    include Test::Helper
+    include Test::Assertion
   end
 end
