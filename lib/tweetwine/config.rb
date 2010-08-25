e8e22b7029279b0e5ff029b8fe85511550a6c6fb
Tuomas Kareinen <tkareine@gmail.com>
2010-08-25 22:14:39 +0300

    Rename StartupConfig to Config, simplify

---
+++ lib/tweetwine/config.rb
@@ -0,0 +1,45 @@
+# coding: utf-8
+
+require "yaml"
+
+module Tweetwine
+  class Config
+    def self.read(args = [], env_lookouts = nil, config_file = nil, default_config = {}, &cmd_option_parser)
+      new(parse_options(args, env_lookouts, config_file, default_config, &cmd_option_parser))
+    end
+
+    def [](key)
+      @options[key]
+    end
+
+    def keys
+      @options.keys
+    end
+
+    private
+
+    def initialize(options)
+      @options = options
+    end
+
+    def self.parse_options(args, env_lookouts, config_file, default_config, &cmd_option_parser)
+      cmd_options  = if cmd_option_parser then cmd_option_parser.call(args) else {} end
+      env_options  = if env_lookouts then parse_env_vars(env_lookouts) else {} end
+      file_options = if config_file && File.exists?(config_file) then parse_config_file(config_file) else {} end
+      default_config.merge(file_options.merge(env_options.merge(cmd_options)))
+    end
+
+    def self.parse_env_vars(env_lookouts)
+      env_lookouts.inject({}) do |result, env_var_name|
+        env_option = ENV[env_var_name.to_s]
+        result[env_var_name.to_sym] = env_option if env_option && !env_option.empty?
+        result
+      end
+    end
+
+    def self.parse_config_file(config_file)
+      options = YAML.load_file(config_file)
+      Util.symbolize_hash_keys(options)
+    end
+  end
+end
---
+++ test/config_test.rb
@@ -0,0 +1,169 @@
+# coding: utf-8
+
+require "test_helper"
+
+module Tweetwine
+
+class ConfigTest < TweetwineTestCase
+  CONFIG_FILE = Helper.fixture_file("test_config.yaml")
+
+  context "when given command line arguments, no environment variables, no config file" do
+    setup do
+      @args = %w{--opt cmd_opt --defopt cmd_defopt left overs}
+      default_config = {:defopt => 'defopt'}
+      @config = Config.read(@args, [], nil, default_config) do |args|
+        args.slice!(0..3)
+        {:opt => 'cmd_opt', :defopt => 'cmd_defopt'}
+      end
+    end
+
+    should "have option defined from command line" do
+      assert_equal 'cmd_opt', @config[:opt]
+    end
+
+    should "override option default value from command line" do
+      assert_equal 'cmd_defopt', @config[:defopt]
+    end
+
+    should "leave remaining command line arguments unconsumed" do
+      assert_equal %w{left overs}, @args
+    end
+  end
+
+  context "when given command line arguments, environment variables, no config file" do
+    setup do
+      ENV['opt']    = 'env_opt'
+      ENV['defopt'] = 'env_defopt'
+      ENV['envopt'] = 'env_envopt'
+      @args = %w{--opt cmd_opt}
+      default_config = {:defopt => 'defopt'}
+      @config = Config.read(@args, [:defopt, :envopt, :opt], nil, default_config) do |args|
+        args.slice!(0..1)
+        {:opt => 'cmd_opt'}
+      end
+    end
+
+    teardown do
+      ENV['opt']    = nil
+      ENV['defopt'] = nil
+      ENV['envopt'] = nil
+    end
+
+    should "have option defined from environment variable" do
+      assert_equal 'env_envopt', @config[:envopt]
+    end
+
+    should "override option value from command line over environment variable" do
+      assert_equal 'cmd_opt', @config[:opt]
+    end
+
+    should "override option default value from environment variable" do
+      assert_equal 'env_defopt', @config[:defopt]
+    end
+  end
+
+  context "when given command line arguments, no environment variables, config file" do
+    setup do
+      @args = %w{--opt cmd_opt}
+      default_config = {:defopt => 'defopt'}
+      @config = Config.read(@args, [], CONFIG_FILE, default_config) do |args|
+        args.slice!(0..1)
+        {:opt => 'cmd_opt'}
+      end
+    end
+
+    should "have option defined from config file" do
+      assert_equal 'file_fileopt', @config[:fileopt]
+    end
+
+    should "override option value from command line over config file" do
+      assert_equal 'cmd_opt', @config[:opt]
+    end
+
+    should "override option default value from config file" do
+      assert_equal 'file_defopt', @config[:defopt]
+    end
+  end
+
+  context "when given command line arguments, environment variables, config file" do
+    setup do
+      @args = %w{--opt2 cmd_opt2}
+      ENV['opt']  = 'env_opt'
+      ENV['opt2'] = 'env_opt2'
+      @config = Config.read(@args, [:opt, :opt2], CONFIG_FILE) do |args|
+        args.slice!(0..2)
+        {:opt2 => 'cmd_opt2'}
+      end
+    end
+
+    teardown do
+      ENV['opt']  = nil
+      ENV['opt2'] = nil
+    end
+
+    should "override option value from environment variable over config file" do
+      assert_equal 'env_opt', @config[:opt]
+    end
+
+    should "override option value from command line over environment variable and config file" do
+      assert_equal 'cmd_opt2', @config[:opt2]
+    end
+  end
+
+  context "when handling command line arguments without parser" do
+    setup do
+      ENV['opt'] = 'env_opt'
+      @args = %w{--opt cmd_opt --defopt cmd_defopt}
+      default_config = {:defopt => 'defopt'}
+      @config = Config.read(@args, [:opt], CONFIG_FILE, default_config)
+    end
+
+    teardown do
+      ENV['opt'] = nil
+    end
+
+    should "ignore command line arguments, using environment variables and config file for options if available" do
+      assert_equal 'env_opt', @config[:opt]
+      assert_equal 'file_defopt', @config[:defopt]
+    end
+  end
+
+  context "when handling environment variables" do
+    setup do
+      ENV['visible'] = 'env_visible'
+      ENV['hidden']  = 'env_hidden'
+      ENV['empty']   = ''
+      @config = Config.read([], [:visible, :empty])
+    end
+
+    teardown do
+      ENV['visible'] = nil
+      ENV['hidden'] = nil
+      ENV['empty'] = nil
+    end
+
+    should "consider only specified environment variables that are nonempty" do
+      assert_equal 'env_visible', @config[:visible]
+    end
+
+    should "not consider empty environment variables" do
+      assert_equal nil, @config[:empty]
+    end
+
+    should "not consider unspecified environment variables" do
+      assert_equal nil, @config[:hidden]
+    end
+  end
+
+  context "when handling nonexistent config file" do
+    setup do
+      @config = Config.read([], [], "no_such_file")
+    end
+
+    should "ignore the config file" do
+      assert @config.keys.empty?
+    end
+  end
+end
+
+end
