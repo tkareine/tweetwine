a1d73cbd6e214e4f8f4be84c3895add163f84e31
Tuomas Kareinen <tkareine@gmail.com>
2010-10-20 04:04:47 +0300

    Code formatting

--- lib/tweetwine/util.rb
+++ lib/tweetwine/util.rb
@@ -19,10 +19,10 @@ module Tweetwine
       difference = (to - from).to_i.abs
 
       value, unit = case difference
-      when 0..59 then [difference, "sec"]
-      when 60..3599 then [(difference/60.0).round, "min"]
-      when 3600..86399 then [(difference/3600.0).round, "hour"]
-      else [(difference/86400.0).round, "day"]
+        when 0..59 then [difference, "sec"]
+        when 60..3599 then [(difference/60.0).round, "min"]
+        when 3600..86399 then [(difference/3600.0).round, "hour"]
+        else [(difference/86400.0).round, "day"]
       end
 
       [value, pluralize_unit(value, unit)]
