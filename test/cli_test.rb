3bf0b8215f359fbd8379b42ca7fbcb469a2e2d44
Tuomas Kareinen <tkareine@gmail.com>
2010-11-11 01:56:30 +0200

    Test that CLI parses options in order
    CLI should allow option to be given multiple times, in which case the
    last value of the option wins.

--- test/cli_test.rb
+++ test/cli_test.rb
@@ -1,6 +1,7 @@
 # coding: utf-8
 
 require "test_helper"
+require "stringio"
 
 module Tweetwine::Test
 
@@ -10,6 +11,20 @@ class CLITest < UnitTestCase
     should "disallow using #new to create a new instance" do
       assert_raise(NoMethodError) { CLI.new }
     end
+
+    should "allow defining same option multiple times, last value winning" do
+      winning_option_value = 'second'
+      start_cli %W{-f first -f #{winning_option_value} -v}
+      assert_equal winning_option_value, CLI.config[:config_file]
+    end
+  end
+
+  private
+
+  def start_cli(args)
+    output = StringIO.new
+    CLI.start(args, { :out => output })
+    output.string.split("\n")
   end
 end
 
