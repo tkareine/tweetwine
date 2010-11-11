1b80b2197b58cdd602cc0a6985f3fe26d5ac7aa5
Tuomas Kareinen <tkareine@gmail.com>
2010-11-11 04:07:17 +0200

    Add integration test for OAuth authorization and config file

--- example/application_behavior_example.rb
+++ example/application_behavior_example.rb
@@ -1,6 +1,9 @@
 # coding: utf-8
 
 require "example_helper"
+require "fixture/oauth"
+
+include Tweetwine::Test::OAuthFixture
 
 Feature "application behavior" do
   in_order_to "know about the application"
@@ -134,6 +137,37 @@ Usage: tweetwine help [<command>]
     Then "the application exists with failure status" do
       @output.should == 'ERROR: unknown command: invalid'
       @status.exitstatus.should == UnknownCommandError.status_code
+    end
+  end
+
+  Scenario "authorize user with OAuth and save access token" do
+    When "I start the application with 'home' command and the command fails due to me being unauthorized" do
+      @command_url = "https://api.twitter.com/1/statuses/home_timeline.json?count=20&page=1"
+      stub_http_request(METHOD, REQUEST_TOKEN_URL).to_return(:body => REQUEST_TOKEN_RESPONSE)
+      stub_http_request(METHOD, ACCESS_TOKEN_URL).to_return(:body => ACCESS_TOKEN_RESPONSE)
+      stub_http_request(:get, @command_url).
+          to_return(:status => [401, 'Unauthorized']).then.
+          to_return(:body => fixture("home.json"))
+      @output = nil
+      @config_contents = nil
+      @config_mode = nil
+      config_file = 'tweetwine.tmp'
+      in_temp_dir do
+        @output = start_cli %W{--no-colors -f #{config_file} home}, [PIN], {}
+        @config_contents = YAML.load_file(config_file)
+        @config_mode = file_mode(config_file)
+      end
+    end
+
+    Then "the application authorizes me, saves access token to config file, and tries the command again" do
+      assert_requested(METHOD, REQUEST_TOKEN_URL)
+      assert_requested(METHOD, ACCESS_TOKEN_URL)
+      assert_requested(:get, @command_url, :headers => {'Authorization' => /^OAuth /}, :times => 2)
+      @output[0].should == "Please authorize: #{AUTHORIZE_URL}"
+      @output[1].should =~ /^Enter PIN:/
+      @output[2].should == "F1-kausi alkaa marraskuussa http://bit.ly/1qQwjQ"
+      @config_contents['oauth_access'].empty?.should == false
+      @config_mode.should == 0600
     end
   end
 end
