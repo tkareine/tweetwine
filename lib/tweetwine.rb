91ddeb703bc55de237610d3d1a0d3cbc119bf495
Tuomas Kareinen <tkareine@gmail.com>
2011-02-20 19:39:46 +0200

    Move exceptions to own file

--- lib/tweetwine.rb
+++ lib/tweetwine.rb
@@ -11,59 +11,9 @@ gem 'oauth', '~> 0.4.4'
 require 'tweetwine/version'
 
 module Tweetwine
-  class Error < StandardError
-    @status_code = 42
-
-    # Idea got from Bundler.
-    def self.status_code(code = nil)
-      return @status_code unless code
-      @status_code = code
-    end
-
-    def status_code
-      self.class.status_code
-    end
-  end
-
-  class CommandLineError    < Error; status_code(13); end
-  class UnknownCommandError < Error; status_code(14); end
-
-  class RequiredOptionError < Error
-    status_code(15)
-
-    attr_reader :key, :owner
-
-    def initialize(key, owner)
-      @key, @owner = key, owner
-    end
-
-    def to_s
-      "#{key} is required for #{owner}"
-    end
-  end
-
-  class ConnectionError     < Error; status_code(21); end
-  class TimeoutError        < Error; status_code(22); end
-
-  class HttpError < Error
-    status_code(25)
-
-    attr_reader :http_code, :http_message
-
-    def initialize(code, message)
-      @http_code, @http_message = code.to_i, message
-    end
-
-    def to_s
-      "#{http_code} #{http_message}"
-    end
-  end
-
-  class TranscodeError      < Error; status_code(31); end
-  class AuthorizationError  < Error; status_code(32); end
-
   lib_path = File.expand_path(File.dirname(__FILE__))
 
+  require "#{lib_path}/tweetwine/exceptions"
   require "#{lib_path}/tweetwine/basic_object"
 
   autoload :CharacterEncoding,  "#{lib_path}/tweetwine/character_encoding"
