a018302c3006bef57f282e283f6081fa7d778636
Tuomas Kareinen <tkareine@gmail.com>
2011-08-16 18:06:21 +0300

    Do not set private methods in mixin modules, case Test::Tweets

--- test/support/tweets.rb
+++ test/support/tweets.rb
@@ -33,8 +33,8 @@ module Tweetwine::Test
     }.freeze
 
     def create_record(fields = {})
-      record = create_nonrt_record(nonrt_fields(fields))
-      modify_to_rt_record(record, fields[:rt_user]) if fields[:rt_user]
+      record = Tweets.create_nonrt_record(Tweets.nonrt_fields(fields))
+      Tweets.modify_to_rt_record(record, fields[:rt_user]) if fields[:rt_user]
       record
     end
 
@@ -50,46 +50,46 @@ module Tweetwine::Test
       (DEFAULT_TIMESTAMP - minus_seconds).iso8601
     end
 
-    private
-
-    def find_hash_of_field_path(hash, path)
-      path = [path] unless path.is_a? Array
-      if path.size > 1
-        hash_path, field = path[0..-2], path.last
-        [Tweetwine::Support.find_hash_path(hash, hash_path), field]
-      else
-        [hash, path.first]
+    class << self
+      def nonrt_fields(fields)
+        DEFAULT_FIELD_VALUES.merge(fields.reject { |(k, v)| k == :rt_user })
       end
-    end
 
-    def nonrt_fields(fields)
-      DEFAULT_FIELD_VALUES.merge(fields.reject { |(k, v)| k == :rt_user })
-    end
-
-    def create_nonrt_record(fields)
-      FIELD_PATHS.inject(deep_copy(RECORD_SKELETON)) do |result, (path_name, path_actual)|
-        hash, field = find_hash_of_field_path(result, path_actual)
-        hash[field] = fields[path_name]
-        result
+      def create_nonrt_record(fields)
+        FIELD_PATHS.inject(deep_copy(RECORD_SKELETON)) do |result, (path_name, path_actual)|
+          hash, field = find_hash_of_field_path(result, path_actual)
+          hash[field] = fields[path_name]
+          result
+        end
       end
-    end
 
-    def modify_to_rt_record(record, rt_user)
-      rt_hash, rt_field = find_hash_of_field_path(record, FIELD_PATHS[:retweet])
-      fr_hash, fr_field = find_hash_of_field_path(record, FIELD_PATHS[:from_user])
-      st_hash, st_field = find_hash_of_field_path(record, FIELD_PATHS[:status])
-      rt_hash[rt_field] = {
-        fr_field => fr_hash[fr_field].dup,
-        'status' => st_hash.dup
-      }
-      fr_hash[fr_field] = rt_user
-      st_hash[st_field] = 'retweeted status text. you should not see me.'
-      cr_hash, cr_field = find_hash_of_field_path(record, FIELD_PATHS[:created_at])
-      cr_hash[cr_field] = 'old created timestamp. you should not see me.'
-    end
+      def modify_to_rt_record(record, rt_user)
+        rt_hash, rt_field = find_hash_of_field_path(record, FIELD_PATHS[:retweet])
+        fr_hash, fr_field = find_hash_of_field_path(record, FIELD_PATHS[:from_user])
+        st_hash, st_field = find_hash_of_field_path(record, FIELD_PATHS[:status])
+        rt_hash[rt_field] = {
+          fr_field => fr_hash[fr_field].dup,
+          'status' => st_hash.dup
+        }
+        fr_hash[fr_field] = rt_user
+        st_hash[st_field] = 'retweeted status text. you should not see me.'
+        cr_hash, cr_field = find_hash_of_field_path(record, FIELD_PATHS[:created_at])
+        cr_hash[cr_field] = 'old created timestamp. you should not see me.'
+      end
 
-    def deep_copy(obj)
-      Marshal.load(Marshal.dump(obj))
+      def find_hash_of_field_path(hash, path)
+        path = [path] unless path.is_a? Array
+        if path.size > 1
+          hash_path, field = path[0..-2], path.last
+          [Tweetwine::Support.find_hash_path(hash, hash_path), field]
+        else
+          [hash, path.first]
+        end
+      end
+
+      def deep_copy(obj)
+        Marshal.load(Marshal.dump(obj))
+      end
     end
   end
 end
